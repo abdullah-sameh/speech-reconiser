cf8a8055088ee04d5401627dca9c9b700e8580ad
abdullah <bdbdsameh00@gmail.com>
2022-10-06 18:54:00 +0200

    initial commit

---
+++ index.js
@@ -0,0 +1,33 @@
+const SpeechRecognition = window.SpeechRecognition || webkitSpeechRecognition;
+const recognition = new SpeechRecognition()
+const lang = document.querySelector('select')
+// console.log(lang.value)
+
+
+recognition.interimResults = true
+recognition.lang = lang.value
+
+lang.addEventListener('change', () => {
+    recognition.lang = lang.value
+})
+
+const words = document.querySelector('.words')
+let p = document.createElement('p')
+words.appendChild(p)
+
+recognition.addEventListener('result', (e) => {
+    const transcript = Array.from(e.results)
+        .map(result => result[0])
+        .map(result => result.transcript)
+        .join('')
+        
+    p.innerText = transcript
+
+    if (e.results[0].isFinal) {
+        p = document.createElement('p')
+        words.appendChild(p)
+    }
+})
+
+recognition.addEventListener('end', recognition.start)
+recognition.start()
